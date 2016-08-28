bf58eb69880cfcebf5a9b3628651331eba388e59
rich1143 <rich1143@morris.umn.edu>
2016-08-27 20:38:59 -0500

    Added comments to extract_and_compile.sh to help me understand

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Two arguments called with this program: the number given as input
+# and the directory used to extract and compile the program.
 NUMBER=$1
 DIRECTORY=$2
 
+# Extract the contents of NthPrime.tgz into the directory.
+# Should create an NthPrime direcory inside this direcory.
 tar zxf NthPrime.tgz --directory $DIRECTORY
 
+# Navigate into the directory just created.
 cd $DIRECTORY/NthPrime
 
+# Compiles and links main.c and nth_prime.c files found
+# in the NthPrime directory.
+# Should create an executable NthPrime.
 gcc -o NthPrime main.c nth_prime.c
 
+# Calls the executable NthPrime with the number that was
+# passed in as an argument.
 ./NthPrime $NUMBER
